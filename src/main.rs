caff9984ff63831ed8733c51574cfd20dba2780d
Samir1984 <allandanielito1984@gmail.com>
2026-07-25 22:30:14 -0600

    Creación de estrucutras bases

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+struct Unidad{
+    numero:i32,
+    nota:f64,
+}
+
+struct Cursos{
+    nombre:String,
+    unidades:[Unidad; 4],
+    nota_para_ganar:f64,
+}
+
+struct Estudiantes{
+    nombre: String,
+    cursos:Vec<Cursos>,
+}
+
 
 fn main() {
     println!("Hello, world!");
