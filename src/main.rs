1801259360bc8612d28b1d08d0504385e6fb0844
Samir1984 <allandanielito1984@gmail.com>
2026-07-25 23:01:44 -0600

    Implementación de métodos para la estructura cursos

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 struct Unidad{
-    numero:i32,
+    letra:String,
     nota:f64,
 }
 
@@ -9,6 +9,31 @@ struct Cursos{
     nota_para_ganar:f64,
 }
 
+impl Cursos{
+    pub fn constructor(nombre:String)->Cursos{
+        Cursos {
+            nombre,
+            unidades: [
+                Unidad { letra: String::from("I"), nota: 0.0 },
+                Unidad { letra: String::from("II"), nota: 0.0 },
+                Unidad { letra: String::from("III"), nota: 0.0 },
+                Unidad { letra: String::from("IV"), nota: 0.0 },
+            ],
+            nota_para_ganar: 240.0,
+        }
+    }
+    pub fn actualizar_notas(&mut self, numero_unidad:String, nota:f64){
+        if let Some(unidad) = self.unidades.iter_mut().find(|u| *u.letra == numero_unidad) 
+        {
+        unidad.nota = nota;
+        }
+    }
+
+    
+}
+
+
+
 struct Estudiantes{
     nombre: String,
     cursos:Vec<Cursos>,
@@ -16,5 +41,7 @@ struct Estudiantes{
 
 
 fn main() {
+
+    
     println!("Hello, world!");
 }
